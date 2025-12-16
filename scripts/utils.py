a0bda3f71baff169b3c611ea8c9fc421adcc5824
Nalaka Wanniarachchi <nalakanayana@gmail.com>
2025-12-17 01:36:18 +0530

    Update utils.py

--- scripts/utils.py
+++ scripts/utils.py
@@ -32,7 +32,8 @@ def fab_authenticate_spn(
         )
 
     #run_fab_command("config set fab_encryption_fallback_enabled true")
-
+    run_fab_command("config set encryption_fallback_enabled true")
+    
     run_fab_command(
         f"auth login -u {client_id} -p {client_secret} --tenant {tenant_id}",
         include_secrets=True,
